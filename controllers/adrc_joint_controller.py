7d4a93758317aef04ede612780a484387fbaeecd
Kacper-So <kacpersobczak152@gmail.com>
2023-05-03 11:53:07 +0200

    + b estimation based on M matrix

--- controllers/adrc_joint_controller.py
+++ controllers/adrc_joint_controller.py
@@ -1,13 +1,14 @@
 import numpy as np
 from observers.eso import ESO
 from .controller import Controller
-
+from models.manipulator_model import ManiuplatorModel
 
 class ADRCJointController(Controller):
     def __init__(self, b, kp, kd, p, q0, Tp):
         self.b = b
         self.kp = kp
         self.kd = kd
+        self.model = ManiuplatorModel(Tp)
 
         A = np.array([[0, 1, 0], [0, 0, 1], [0, 0, 0]])
         B = np.array([[0], [b], [0]])
@@ -17,10 +18,10 @@ class ADRCJointController(Controller):
 
     def set_b(self, b):
         self.b = b
-        B = np.transpose(np.array([0, b, 0]))
+        B = np.array([[0], [b], [0]])
         self.eso.set_B(B)
 
-    def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
+    def calculate_control(self, x, q_d, q_d_dot, q_d_ddot, index):
         ### TODO implement ADRC
         est = self.eso.get_state()
         q_dot_est = est[1]
@@ -30,4 +31,10 @@ class ADRCJointController(Controller):
         v = q_d_ddot + self.kd * e_dot + self.kp * e
         u = (v - f) / self.b
         self.eso.update(x[0], u)
+        x_copy = x
+        x_copy.append(0.)
+        x_copy.append(0.)
+        M = self.model.M(x_copy)
+        M = np.linalg.inv(M)
+        self.set_b(M[index, index])
         return u
